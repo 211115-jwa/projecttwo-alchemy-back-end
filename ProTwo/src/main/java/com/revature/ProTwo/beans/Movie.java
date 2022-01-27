7b1ab42f1f7c71eff7becbc97f1e7dff5b024311
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-01-27 13:54:16 -0500

    fixede movie and review and UR issues

--- ProTwo/src/main/java/com/revature/ProTwo/beans/Movie.java
+++ ProTwo/src/main/java/com/revature/ProTwo/beans/Movie.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Movie {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String movieName;
